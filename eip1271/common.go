e8a99725d9ca45e089aa497bc49cca2b6ba48fe5
oleg-ssvlabs <oleg.iar@ssvlabs.io>
2026-04-09 09:15:27 +0200

    Merge pull request #29 from ssvlabs/fix/eip1271-signature
    fix(EIP-1271) - change function parameter type and selector

--- eip1271/common.go
+++ eip1271/common.go
@@ -10,11 +10,11 @@ import (
 // EIP-1271 magic values (https://eips.ethereum.org/EIPS/eip-1271)
 // The four-byte code is defined as follows:
 // - 0x00000000 if the signature is invalid.
-// - 0x20c13b0b if the signature is valid and was produced using the eth_sign method.
-// - 0x1626ba7e if the signature is valid and was produced using the personal_sign method.
+// - 0x1626ba7e if the signature is valid (standard EIP-1271, selector of isValidSignature(bytes32,bytes)).
 
-var MAGIC_VALUE_ETH_SIGN = [4]byte{0x16, 0x26, 0xba, 0x7e}
-var MAGIC_VALUE_PERSONAL_SIGN = [4]byte{0x20, 0xc1, 0x3b, 0x0b}
+// MAGIC_VALUE is the standard EIP-1271 magic value (0x1626ba7e).
+var MAGIC_VALUE = [4]byte{0x16, 0x26, 0xba, 0x7e}
+
 var InvalidSigValue = [4]byte{0xff, 0xff, 0xff, 0xff}
 
 type ETHClient interface {
